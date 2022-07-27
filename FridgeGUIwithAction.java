93ed70010ea98524d3418f2b3ca30dda783d3dee
Cuichan Wu <wu.cu@northeastern.edu>
2022-07-26 20:03:18 -0700

    Update color

--- FridgeGUIwithAction.java
+++ FridgeGUIwithAction.java
@@ -11,6 +11,10 @@ public class FridgeGUIwithAction extends JFrame implements ActionListener  {
     public static final int WIDTH = 350;
     public static final int HEIGHT = 750;
     private ActionEvent e;
+    public static final Color mainBackground = new Color(242, 245, 237);
+    public static final Color menuBackground = new Color(232, 240, 213);
+    public static final Color titleColor = new Color(122, 156, 87);
+    public static final Color redColor = new Color(223, 185, 182);
 
 
     public FridgeGUIwithAction(){
@@ -18,29 +22,28 @@ public class FridgeGUIwithAction extends JFrame implements ActionListener  {
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-        Color menuColor = new Color(107,174,120);
 
         //top menu
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(1,3));
-        buttonPanel.setBackground(menuColor);
+        buttonPanel.setBackground(menuBackground);
 
         JButton addButton = new JButton("➕ Add");
-        addButton.setBackground(menuColor);
+        addButton.setBackground(menuBackground);
         addButton.setOpaque(true);
         addButton.setBorderPainted(false);
         addButton.addActionListener(this);
         buttonPanel.add(addButton);
 
         JButton viewButton = new JButton("👁 Stock");
-        viewButton.setBackground(menuColor);
+        viewButton.setBackground(menuBackground);
         viewButton.setOpaque(true);
         viewButton.setBorderPainted(false);
         viewButton.addActionListener(this);
         buttonPanel.add(viewButton);
 
         JButton recipeButton = new JButton("📖 Recipe");
-        recipeButton.setBackground(menuColor);
+        recipeButton.setBackground(menuBackground);
         recipeButton.setOpaque(true);
         recipeButton.setBorderPainted(false);
         recipeButton.addActionListener(this);
@@ -56,30 +59,33 @@ public class FridgeGUIwithAction extends JFrame implements ActionListener  {
         lowStock.setLayout(new BorderLayout());
         JLabel titleLow = new JLabel(" Low Stock");
         titleLow.setFont(new Font("Arial", Font.LAYOUT_LEFT_TO_RIGHT, 18));
-        titleLow.setForeground(new Color(15,118,36));
+        titleLow.setForeground(titleColor);
+        lowStock.setBackground(mainBackground);
         lowStock.add(titleLow, BorderLayout.NORTH);
         //details put in center/bottom -- not finished yet
-        lowStock.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        lowStock.setBorder(BorderFactory.createLineBorder(Color.GRAY));
         infoPanel.add(lowStock);
 
         JPanel expired = new JPanel();
         expired.setLayout(new BorderLayout());
         JLabel titleExpired = new JLabel(" Expired");
         titleExpired.setFont(new Font("Arial", Font.LAYOUT_LEFT_TO_RIGHT, 18));
-        titleExpired.setForeground(new Color(15,118,36));
+        titleExpired.setForeground(titleColor);
+        expired.setBackground(mainBackground);
         expired.add(titleExpired, BorderLayout.NORTH);
         //details put in center/bottom -- not finished yet
-        expired.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        expired.setBorder(BorderFactory.createLineBorder(Color.GRAY));
         infoPanel.add(expired);
 
         JPanel expiring = new JPanel();
         expiring.setLayout(new BorderLayout());
         JLabel titleExpiring = new JLabel(" Expiring soon");
         titleExpiring.setFont(new Font("Arial", Font.LAYOUT_LEFT_TO_RIGHT, 18));
-        titleExpiring.setForeground(new Color(15,118,36));
+        titleExpiring.setForeground(titleColor);
+        expiring.setBackground(mainBackground);
         expiring.add(titleExpiring, BorderLayout.NORTH);
         //details put in center/bottom -- not finished yet
-        expiring.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        expiring.setBorder(BorderFactory.createLineBorder(Color.GRAY));
         infoPanel.add(expiring);
 
         add(infoPanel, BorderLayout.CENTER);
