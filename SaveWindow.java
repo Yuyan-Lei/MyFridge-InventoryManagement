cdbe52dcaf05c2dd3c0cd26222fcfc47c251a727
xuxiaoxuxiao <106841104+xuxiaoxuxiao@users.noreply.github.com>
2022-07-28 23:53:20 -0700

    Add files via upload

---
+++ SaveWindow.java
@@ -0,0 +1,103 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+
+public class SaveWindow extends JFrame implements ActionListener {
+
+    SaveWindow() throws ParseException {
+        DefaultUI ui = new DefaultUI("Item(s) Saved", this);
+        setVisible(true);
+
+        // 2. Bottom Bar
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(1,4));
+        buttonPanel.setBackground(FridgeGUIwithAction.MENU_BACKGROUND);
+
+        ImageIcon addIcon = new ImageIcon("./icons/add.png");
+        Image img = addIcon.getImage();
+        Image newImg = img.getScaledInstance(FridgeGUIwithAction.ICON_SIZE, FridgeGUIwithAction.ICON_SIZE, Image.SCALE_SMOOTH);
+        addIcon = new ImageIcon(newImg);
+        JButton addButton = new JButton(addIcon);
+        addButton.setActionCommand("add");
+        addButton.setBackground(FridgeGUIwithAction.MENU_BACKGROUND);
+        addButton.setOpaque(true);
+        addButton.setBorderPainted(false);
+        addButton.addActionListener(this);
+        buttonPanel.add(addButton);
+
+        ImageIcon notificationIcon = new ImageIcon("./icons/expired.png");
+        img = notificationIcon.getImage();
+        newImg = img.getScaledInstance(FridgeGUIwithAction.ICON_SIZE, FridgeGUIwithAction.ICON_SIZE, Image.SCALE_SMOOTH);
+        notificationIcon = new ImageIcon(newImg);
+        JButton notificationButton = new JButton(notificationIcon);
+        notificationButton.setActionCommand("notification");
+        notificationButton.setBackground(FridgeGUIwithAction.MENU_BACKGROUND);
+        notificationButton.setOpaque(true);
+        notificationButton.setBorderPainted(false);
+        notificationButton.addActionListener(this);
+        buttonPanel.add(notificationButton);
+
+        ImageIcon viewIcon = new ImageIcon("./icons/stock.png");
+        img = viewIcon.getImage();
+        newImg = img.getScaledInstance(FridgeGUIwithAction.ICON_SIZE, FridgeGUIwithAction.ICON_SIZE, Image.SCALE_SMOOTH);
+        viewIcon = new ImageIcon(newImg);
+        JButton viewButton = new JButton(viewIcon);
+        viewButton.setActionCommand("stock");
+        viewButton.setBackground(FridgeGUIwithAction.MENU_BACKGROUND);
+        viewButton.setOpaque(true);
+        viewButton.setBorderPainted(false);
+        viewButton.addActionListener(this);
+        buttonPanel.add(viewButton);
+
+        ImageIcon recipeIcon = new ImageIcon("./icons/recipe_g.png");
+        img = recipeIcon.getImage();
+        newImg = img.getScaledInstance(FridgeGUIwithAction.ICON_SIZE, FridgeGUIwithAction.ICON_SIZE, Image.SCALE_SMOOTH);
+        recipeIcon = new ImageIcon(newImg);
+        JButton recipeButton = new JButton(recipeIcon);
+        recipeButton.setActionCommand("recipe");
+        recipeButton.setBackground(FridgeGUIwithAction.MENU_BACKGROUND);
+        recipeButton.setOpaque(true);
+        recipeButton.setBorderPainted(false);
+        recipeButton.addActionListener(this);
+        buttonPanel.add(recipeButton);
+
+        add(buttonPanel, BorderLayout.SOUTH);
+
+        //Central body
+        JLabel message = new JLabel("Success!");
+        add(message,BorderLayout.CENTER);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        String actionCommand = e.getActionCommand();
+        if (actionCommand.equals("add")) {
+            setVisible(false); //can keep the new window opened only (looks like close the previous window)
+            try {
+                AddWindow aNewWindow = new AddWindow();
+            } catch (ParseException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        else if (actionCommand.equals("notification")) {
+            setVisible(false);
+            try {
+                FridgeGUIwithAction aNewWindow = new FridgeGUIwithAction();
+                aNewWindow.setVisible(true);
+            } catch (ParseException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        else if (actionCommand.equals("stock")) {
+            setVisible(false);
+            try {
+                StockWindow aNewWindow = new StockWindow();
+            } catch (ParseException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        else
+            System.out.println("Unexpected error.");
+    }
+}
