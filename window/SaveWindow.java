5c87ca01cd95c01fe43a4f8cb617afad46f6c7df
xuxiaoxuxiao <xiaoxu2016@gmail.com>
2022-08-03 22:39:08 -0700

    save window modified

--- window/SaveWindow.java
+++ window/SaveWindow.java
@@ -12,19 +12,34 @@ public class SaveWindow extends JFrame {
 
         //Central body - saveIcon
         JPanel savePanel = new JPanel(new GridBagLayout());
-        savePanel.setBackground(DefaultUI.WHITE_COLOR);
+        savePanel.setBackground(window.DefaultUI.WHITE_COLOR);
+        JPanel saveIconPanel = new JPanel(new GridBagLayout());
+        saveIconPanel.setBackground(DefaultUI.WHITE_COLOR);
         JLabel message = new JLabel();
-        savePanel.add(message);
-        add(savePanel,BorderLayout.CENTER);
+        saveIconPanel.add(message);
+        add(saveIconPanel,BorderLayout.CENTER);
         ImageIcon saveIcon = new ImageIcon("icons/basicIcons/saved.png");
         Image saveImg = saveIcon.getImage();
         saveImg = saveImg.getScaledInstance(150, 150, Image.SCALE_SMOOTH);
         saveIcon = new ImageIcon(saveImg);
         message.setIcon(saveIcon);
 
+        JPanel saveLabelPanel = new JPanel(new GridBagLayout());
+        saveLabelPanel.setBackground(window.DefaultUI.WHITE_COLOR);
         JLabel savedLabel = new JLabel("Saved!");
-        savedLabel.setBackground(window.DefaultUI.WHITE_COLOR);
-        savedLabel.setFont(new Font("Saved!", Font.PLAIN, 14));
+        savedLabel.setFont(new Font("Saved!", Font.PLAIN, 21));
+        saveLabelPanel.add(savedLabel);
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        savePanel.add(saveIconPanel,c);
+        c.gridx = 0;
+        c.gridy = 1;
+        savePanel.add(saveLabelPanel,c);
         add(savePanel);
+
+        //Timer timer = new Timer(5000, new AddWindow());
     }
+    
 }
